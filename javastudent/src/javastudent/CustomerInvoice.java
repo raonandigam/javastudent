f04dcf0db63a17597e74449b8cbb41136d40ce4b
Ckanchi <Ckanchi@L-SFF16JZN72.americas.gap.com>
2017-03-12 20:24:22 -0700

    my final commit-setters and getters

--- javastudent/src/javastudent/CustomerInvoice.java
+++ javastudent/src/javastudent/CustomerInvoice.java
@@ -5,5 +5,30 @@ public class CustomerInvoice {
 	 String item ;
 	 double price ;
 	 String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	 
 	
 }
--- javastudent/src/javastudent/Invoice.java
+++ javastudent/src/javastudent/Invoice.java
@@ -8,36 +8,36 @@ public static void main(String[] args){
 	   CustomerInvoice invoice1 = new CustomerInvoice();
 	   CustomerInvoice invoice3 = new CustomerInvoice();
 	     
-	     invoice.name = "chaitanya"; ;
-		 invoice.item = "iphone";
-		 invoice.price = 800.00;
-		 invoice.address = "40640,high street,apt#802,fremont,94538,ca";
+	     invoice.setName ( "chaitanya") ;
+		 invoice.setItem ("iphone");
+		 invoice.setPrice (800.00);
+		 invoice.setAddress ( "40640,high street,apt#802,fremont,94538,ca");
 		 
-		 invoice1.name = "vasu"; ;
-		 invoice1.item = "samsungphone";
-		 invoice1.price = 500.00;
-		 invoice1.address = "40640,high street,apt#802,fremont,94538,ca";
+		 invoice1.setName ( "vasu") ;
+		 invoice1.setItem ( "samsungphone");
+		 invoice1.setPrice ( 500.00);
+		 invoice1.setAddress  ("40640,high street,apt#802,fremont,94538,ca");
 		 
 		 
-		 invoice3.name = "chaitanya"; ;
-		 invoice3.item = "iphone";
-		 invoice3.price = 800.00;
-		 invoice3.address = "40640,high street,apt#802,fremont,94538,ca";
+		 invoice3.setName ( "krishna") ;
+		 invoice3.setItem ("nokia");
+		 invoice3.setPrice (800.00);
+		 invoice3.setAddress ( "40640,high street,apt#802,fremont,94538,ca");
 		 
-		System.out.println("name is:" +invoice.name);
-		System.out.println("item is:" +invoice.item);
-		System.out.println("price is:" +invoice.price);
-		System.out.println("address is:" +invoice.address);
+		System.out.println("name is:" +invoice.getName());
+		System.out.println("item is:" +invoice.getItem());
+		System.out.println("price is:" +invoice.getPrice());
+		System.out.println("address is:" +invoice.getAddress());
 		System.out.println("--------------------------");
-		System.out.println("name is:" +invoice1.name);
-		System.out.println("item is:" +invoice1.item);
-		System.out.println("price is:" +invoice1.price);
-		System.out.println("address is:" +invoice1.address);
+		System.out.println("name is:" +invoice1.getName());
+		System.out.println("item is:" +invoice1.getItem());
+		System.out.println("price is:" +invoice1.getPrice());
+		System.out.println("address is:" +invoice1.getAddress());
 		System.out.println("--------------------------");
-		System.out.println("name is:" +invoice3.name);
-		System.out.println("item is:" +invoice3.item);
-		System.out.println("price is:" +invoice3.price);
-		System.out.println("address is:" +invoice3.address);
+		System.out.println("name is:" +invoice3.getName());
+		System.out.println("item is:" +invoice3.getItem());
+		System.out.println("price is:" +invoice3.getPrice());
+		System.out.println("address is:" +invoice3.getAddress());
 		
 		
 		
