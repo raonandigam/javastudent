8465e89b9aabddee08af12a69657734e5fa9140b
Ckanchi <Ckanchi@L-SFF16JZN72.americas.gap.com>
2017-03-12 19:52:19 -0700

    my thirsd commit

--- javastudent/src/javastudent/CustomerInvoice.java
+++ javastudent/src/javastudent/CustomerInvoice.java
@@ -1,20 +1,9 @@
 package javastudent;
 
 public class CustomerInvoice {
-	 String name = "chaitanya"; ;
-	 String item = "iphone";
-	 double price = 800.00;
-	 String address = "40640,high street,apt#802,fremont,94538,ca";;
+	 String name;
+	 String item ;
+	 double price ;
+	 String address;
 	
-	 void disply(){
-		
-		System.out.println("customer name :" +name);
-	    System.out.println("item :" +item);
-	    System.out.println("item price :" +price);
-	    System.out.println("customer address :" +address);
-	    
-	    
-		
-	}
-
 }
--- javastudent/src/javastudent/Invoice.java
+++ javastudent/src/javastudent/Invoice.java
@@ -1,10 +1,20 @@
 package javastudent;
 
 public class Invoice {
-
+	 String name;
+	 String item ;
+	 double price ;
+	 String address;
 	public static void main(String[] args){
-	 
-		CustomerInvoice invoice = new CustomerInvoice();
-        invoice.disply();
+	   CustomerInvoice invoice = new CustomerInvoice();
+		 invoice.name = "chaitanya"; ;
+		 invoice.item = "iphone";
+		 invoice.price = 800.00;
+		 invoice.address = "40640,high street,apt#802,fremont,94538,ca";
+		System.out.println("name is:" +invoice.name);
+		System.out.println("item is:" +invoice.item);
+		System.out.println("price is:" +invoice.price);
+		System.out.println("address is:" +invoice.address);
+		
 	}
-	}
+}
